371b2b1e65e4f34ae7e4f4f7f9edffdc8f0c3724
Roy McFarland <roy.mcfarland@gmail.com>
2015-04-16 20:06:30 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var a127 = require('a127-magic');
+var express = require('express');
+var app = express();
+
+module.exports = app; // for testing
+
+// initialize a127 framework
+a127.init(function(config) {
+
+  // include a127 middleware
+  app.use(a127.middleware(config));
+
+  // error handler to emit errors as a json string
+  app.use(function(err, req, res, next) {
+    if (err && typeof err === 'object') {
+      Object.defineProperty(err, 'message', { enumerable: true }); // include message property in response
+      res.end(JSON.stringify(err));
+    }
+    next(err);
+  });
+  var port = process.env.PORT || 10010;
+  // begin listening for client requests
+  app.listen(port);
+
+  console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Boulder');
+});
